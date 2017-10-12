2a281dfe964e741ca0c44166eca8e257b3347f0d
hjaltielias <hjalti@heimkaup.is>
2017-10-12 13:33:56 +0000

    Linting is overkill for this but ok

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-ie-flex-basis-default-auto', function (opts) {
-    opts = opts || {};
-
-    return function (root, result) {
+module.exports = postcss.plugin('postcss-ie-flex-basis-default-auto', function () {
+    return function (root) {
         root.walkDecls('-ms-flex', declaration => {
             const match = declaration.value.match(/^\s*(\d+)(?:\s+(\d+))?\s*$/);
 
             if (match) {
-                declaration.parent.append({ prop: '-ms-flex-preferred-size', value: 'auto' });
+                declaration.parent.append({
+                    prop: '-ms-flex-preferred-size',
+                    value: 'auto'
+                });
             }
         });
     };
